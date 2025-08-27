fb9745bc669968505507467f266de7b2d9ce425c
hamosapiens <49108036+hamosapiens@users.noreply.github.com>
2025-08-27 16:48:27 -0700

    improvements and copy update2

--- components/RichTextPB.tsx
+++ components/RichTextPB.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 export function RichTextPB({ text }: { text?: string }) {
   if (!text) return null;
   return (
-    <div className="text-white/80 text-sm text-pretty sm:text-base text-pretty tracking-tight leading-relaxed whitespace-pre-line">
+    <div className="text-white/80 text-sm text-pretty sm:text-base tracking-tight leading-relaxed whitespace-pre-line">
       {text}
     </div>
   );
